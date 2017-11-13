31758a5e6da10ac9b0645e748bc8a5e0794fd9f2
GianIsAlive <gianthedesigner@gmail.com>
2017-11-12 22:22:06 -0800

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        }),
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'file-loader',
+        options: {
+          name: './img/[name].[ext]',
+        },
+      },
+      {
+        test: /\.(woff|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'base64-font-loader',
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('./style/style.css'),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
