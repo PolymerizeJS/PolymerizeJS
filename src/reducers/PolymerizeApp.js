867128499721bc41365b050c2e13e317a6100189
GianIsAlive <gianthedesigner@gmail.com>
2017-11-14 00:39:43 -0800

    root reducer

--- src/reducers/PolymerizeApp.js
+++ src/reducers/PolymerizeApp.js
@@ -1,8 +1,6 @@
-/*
-{
-  UIState: {
-    
-  }
-}
+import { combineReducers } from 'redux';
+import onboardDisplay from './onboardDisplay';
 
-*/
+const polymerizeApp = combineReducers({ onboardDisplay });
+
+export default polymerizeApp;
