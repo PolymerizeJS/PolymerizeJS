28982b495f12538b12c943c8539d1716b86d03d4
GianIsAlive <gianthedesigner@gmail.com>
2017-11-14 00:37:42 -0800

    add container for button

--- src/components/Shared/SmallButton/SmallButton.jsx
+++ src/components/Shared/SmallButton/SmallButton.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './SmallButtonStyle.scss';
 
-function SmallButton() {
+function SmallButton({ smallButtonContent }) {
   return (
-    <button className="small-button">
-      props.SmallButtonContent
+    <button
+      className="small-button"
+    >
+      { smallButtonContent }
     </button>
   );
 }
+
+SmallButton.propTypes = {
+  smallButtonContent: PropTypes.string.isRequired,
+};
 
 export default SmallButton;
