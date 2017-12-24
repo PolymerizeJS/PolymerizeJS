71518cb2a5803dba529a7e6ac231e93aae83e53a
GianIsAlive <gianthedesigner@gmail.com>
2017-12-23 22:56:27 -0500

    hydrate component from parent component

--- src/PageComponent/LandingPage/index.jsx
+++ src/PageComponent/LandingPage/index.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 
-import HeaderBlock from '../../TemplateComponent/HeaderBlock/HeaderBlock';
 import BannerBlock from '../../TemplateComponent/BannerBlock/BannerBlock';
 import ContentBlock from '../../TemplateComponent/ContentBlock/ContentBlock';
 
 import './style.scss';
 
 function LandingPage() {
+  const contentBlockData = [
+    {
+      primaryTextContent: 'Alinea',
+      secondaryTextContent: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+      buttonName: 'Get it on Github',
+    },
+    {
+      primaryTextContent: 'Apollo',
+      secondaryTextContent: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+      buttonName: 'Get it on Github',
+    },
+  ];
+  const ContentBlockMap = contentBlockData.map((el, idx) => {
+    return <ContentBlock key={idx.toString()}>{el}</ContentBlock>;
+  });
   return (
     <section className="landing-page">
       {/* <HeaderBlock /> */}
       <BannerBlock />
-      <ContentBlock />
+      {ContentBlockMap}
     </section>
   );
 }
