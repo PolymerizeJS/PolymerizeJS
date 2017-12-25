90f9a2b7785ed12472e59e68c1e4e7bd75e26eea
GianIsAlive <gianthedesigner@gmail.com>
2017-12-25 14:51:08 -0500

    fix contact form reducer

--- src/TemplateComponent/ContactFormBlock/ContactFormBlockView.jsx
+++ src/TemplateComponent/ContactFormBlock/ContactFormBlockView.jsx
@@ -4,22 +4,44 @@ import SecondaryText from '../../AtomComponent/SecondaryText';
 import SmallText from '../../AtomComponent/SmallText';
 import HalfWidthInput from '../../AtomComponent/HalfWidthInput';
 import HalfWidthTextarea from '../../AtomComponent/HalfWidthTextarea';
+import MediumButton from '../../AtomComponent/MediumButton';
+
+import {
+  handleName,
+  handleSubject,
+  handleEmail,
+  handleMessage,
+  handleSend,
+} from './ContactFormBlockAction';
 
 import './style.scss';
 
-function ContactFormBlockView() {
+function ContactFormBlockView({ dispatch, name, subject, email, message }) {
   return (
     <section className="contact-form-block">
       <SecondaryText>Contact Us</SecondaryText>
-      <form className="contact-form" action="POST">
+      <form className="contact-form">
         <SmallText>Name:</SmallText>
-        <HalfWidthInput />
+        <HalfWidthInput
+          onChange={e => dispatch(handleName(e.target.value))}
+          value={name}
+        />
         <SmallText>Subject:</SmallText>
-        <HalfWidthInput />
+        <HalfWidthInput
+          onChange={e => dispatch(handleSubject(e.target.value))}
+          value={subject}
+        />
         <SmallText>Email:</SmallText>
-        <HalfWidthInput />
-        <SmallText>Content:</SmallText>
-        <HalfWidthTextarea />
+        <HalfWidthInput
+          onChange={e => dispatch(handleEmail(e.target.value))}
+          value={email}
+        />
+        <SmallText>Message:</SmallText>
+        <HalfWidthTextarea
+          onChange={e => dispatch(handleMessage(e.target.value))}
+          value={message}
+        />
+        <MediumButton>Send</MediumButton>
       </form>
     </section>
   );
