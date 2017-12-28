9ce391141983a8bdb2ae475ed9b84ff8154af98a
GianIsAlive <gianthedesigner@gmail.com>
2017-12-27 20:10:56 -0500

    add header block

---
+++ src/TemplateComponent/HeaderBlock/HeaderBlock.jsx
@@ -0,0 +1,7 @@
+import { connect } from 'react-redux';
+
+import HeaderBlockView from './HeaderBlockView';
+
+const HeaderBlock = connect()(HeaderBlockView);
+
+export default HeaderBlock;
--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 
 // reducers
+import headerBlockReducer from './TemplateComponent/HeaderBlock/headerBlockReducer';
 import bannerBlockReducer from './TemplateComponent/BannerBlock/bannerBlockReducer';
 import contentBlockReducer from './TemplateComponent/ContentBlock/contentBlockReducer';
 import contactFormBlockReducer from './TemplateComponent/ContactFormBlock/contactFormBlockReducer';
 
 const PolymerizeApp = combineReducers({
+  headerBlockReducer,
   bannerBlockReducer,
   contentBlockReducer,
   contactFormBlockReducer,
